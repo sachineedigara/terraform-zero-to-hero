3ff5dd5120ab5564228880735ae4468411ae4974
sachineedigara <147805678+sachineedigara@users.noreply.github.com>
2024-07-02 20:01:52 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,3 +6,16 @@ resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
+======================
+
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-04b70fa74e45c3917"  
+    instance_type = "t2.micro"
+    subnet_id = "subnet-036343e5f477f5a2e"
+    key_name = "terraform"
+}
